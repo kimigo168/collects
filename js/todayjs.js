ca5fec8dc745384f5cc91310aa739357d1880a9c
kimigo168 <462494169@qq.com>
2018-09-07 18:23:49 +0800

    9-7

--- js/todayjs.js
+++ js/todayjs.js
@@ -60,3 +60,69 @@ $.check_ios_ad({
 // 4.substr() 方法可在字符串中抽取从 start 下标开始的指定数目的字符。
 // stringObject.substr(start,length) substring() vs substr()
 //5.for of for in 
+
+// 6.拍照调用
+
+// 开始拍照
+var mediaStreamTrack;
+function startPic() {
+  let that = this;
+  that.mediaStreamTrack = '';
+  if (navigator.mediaDevices === undefined) { // 针对部分浏览器不支持mediaDevices
+    navigator.mediaDevices = {};
+  }
+  
+  if (navigator.mediaDevices.getUserMedia === undefined) {
+    navigator.mediaDevices.getUserMedia = function (constraints) {
+      var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+      if (!getUserMedia) {
+        return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+      }
+      return new Promise(function(resolve, reject) {
+        getUserMedia.call(navigator, constraints, resolve, reject);
+      });
+    };
+  }
+
+  navigator.mediaDevices.getUserMedia({audio: true, video: {width:320, height: 320}}).then(stream => {
+    mediaStreamTrack = typeof stream.stop === 'function' ? stream : stream.getTracks()[1];
+    var video = document.querySelector('video');
+    if ('srcObject' in video) {
+      video.srcObject = stream;
+    } else {
+      video.src = (window.URL || window.webkitURL).createObjectURL(stream);
+    }
+    video.onloadedmetadata = function (e) {
+      video.play();
+    };
+  }).catch(err => {
+    console.log(err);
+    alert('调取摄像头失败');
+  });
+}
+// 拍照
+function shotPic() {
+  var video = document.querySelector("#video");
+  var context = document.getElementById("canvas").getContext("2d");
+  context.drawImage(video, 0, 0, 300, 400);
+}
+
+//停止拍照
+function stopPic() {
+  mediaStreamTrack && mediaStreamTrack.stop();
+}
+// 将照片上传服务器
+function uploadPic() {
+  var imgData=document.getElementById("canvas").toDataURL("image/png");
+  var mmmd=getBase64Image(document.getElementById("mmm"));
+
+}
+function getBase64Image(img) {
+  var canvastemp = document.createElement("canvas");
+  canvastemp.width = img.width;
+  canvastemp.height = img.height;
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0, img.width, img.height);
+  var dataURL = canvas.toDataURL("image/png");
+  return dataURL
+}

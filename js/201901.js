8c459af888bf5aa1409e0431ad237464c3a4ee95
kimigo168 <462494169@qq.com>
2019-01-30 18:23:15 +0800

    add apply

--- js/201901.js
+++ js/201901.js
@@ -297,3 +297,109 @@ function sub (a, b) {
   return a - b;
 }
 
+// 27.call,apply,bind区别
+
+// (1) apply和bind,改变函数执行时的上下文，将一个对象的方法交给另一个对象来执行，并且是立即执行的。
+// (2) 调用 call 和 apply 的对象，必须是一个函数 Function
+
+// call 写法：
+// Function.call(obj,[param1[,param2[,…[,paramN]]]])
+
+// 调用 call 的对象，必须是个函数 Function。
+// call 的第一个参数，是一个对象。 Function 的调用者，将会指向这个对象。如果不传，则默认为全局对象 window。
+// 第二个参数开始，可以接收任意个参数。每个参数会映射到相应位置的 Function 的参数上。
+// 但是如果将所有的参数作为数组传入，它们会作为一个整体映射到 Function 对应的第一个参数上，之后参数都为空。
+
+function func (a,b,c) {}
+
+func.call(obj, 1,2,3)
+// func 接收到的参数实际上是 1,2,3
+
+func.call(obj, [1,2,3])
+// func 接收到的参数实际上是 [1,2,3],undefined,undefined
+
+// apply写法:
+// Function.apply(obj[,argArray])
+
+// 它的调用者必须是函数 Function，并且只接收两个参数，第一个参数的规则与 call 一致。
+// 第二个参数，必须是数组或者类数组，它们会被转换成类数组，传入 Function 中，并且会被映射到 Function 对应的参数上。这也是 call 和 apply 之间，很重要的一个区别。
+
+func.apply(obj, [1,2,3])
+// func 接收到的参数实际上是 1,2,3
+
+func.apply(obj, {
+    0: 1,
+    1: 2,
+    2: 3,
+    length: 3
+})
+// func 接收到的参数实际上是 1,2,3
+
+// call应用场景：
+// 1、对象的继承。如下面这个例子：
+function superClass () {
+  this.a = 1;
+  this.print = function () {
+      console.log(this.a);
+  }
+}
+
+function subClass () {
+  superClass.call(this);
+  this.print();
+}
+
+subClass();
+// 1
+// subClass 通过 call 方法，继承了 superClass 的 print 方法和 a 变量。此外，subClass 还可以扩展自己的其他方法。
+
+// 2、借用方法。还记得刚才的类数组么？如果它想使用 Array 原型链上的方法，可以这样：
+let domNodes = Array.prototype.slice.call(document.getElementsByTagName("*"));
+// 这样，domNodes 就可以应用 Array 下的所有方法了。
+
+// apply 的一些妙用
+// 1、Math.max。用它来获取数组中最大的一项。
+let max = Math.max.apply(null, array);
+// 同理，要获取数组中最小的一项，可以这样：
+let min = Math.min.apply(null, array);
+// 2、实现两个数组合并。在 ES6 的扩展运算符出现之前，我们可以用 Array.prototype.push来实现。
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+
+Array.prototype.push.apply(arr1, arr2);
+console.log(arr1); // [1, 2, 3, 4, 5, 6]
+
+// bind的使用
+// bind() 方法创建一个新的函数，在调用时设置 this 关键字为提供的值。并在调用新函数时，将给定参数列表作为原函数的参数序列的前若干项。
+// Function.bind(thisArg[, arg1[, arg2[, ...]]])
+// bind 方法 与 apply 和 call 比较类似，也能改变函数体内的 this 指向。不同的是，bind 方法的返回值是函数，并且需要稍后调用，才会执行。而 apply 和 call 则是立即调用。
+
+function add (a, b) {
+  return a + b;
+}
+
+function sub (a, b) {
+  return a - b;
+}
+
+add.bind(sub, 5, 3); // 这时，并不会返回 8
+add.bind(sub, 5, 3)(); // 调用后，返回 8
+
+// 如果 bind 的第一个参数是 null 或者 undefined，this 就指向全局对象 window。
+
+// 总结
+// call 和 apply 的主要作用，是改变对象的执行上下文，并且是立即执行的。它们在参数上的写法略有区别。
+// bind 也能改变对象的执行上下文，它与 call 和 apply 不同的是，返回值是一个函数，并且需要稍后再调用一下，才会执行。
+
+// 猫吃鱼，狗吃肉，奥特曼打小怪兽。
+// 有天狗想吃鱼了
+
+// 猫.吃鱼.call(狗，鱼)
+
+// 狗就吃到鱼了
+
+// 猫成精了，想打怪兽
+
+// 奥特曼.打小怪兽.call(猫，小怪兽)
+
+// 猫也可以打小怪兽了

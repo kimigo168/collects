5d4dd0cb4df51f666282d62cc2c5d45d65d539a2
kimigo168 <462494169@qq.com>
2019-08-29 18:04:14 +0800

    proxy

--- js/es6.js
+++ js/es6.js
@@ -129,4 +129,96 @@ var p = new Point(); // p { x: 0, y: 0 }
 var arr1 = [1, 2, 3]
 var arr2 = [4, 5, 6]
 arr1.push(...arr2)
-// P79 
+// P80 箭头函数
+const full = ({first, last}) => first + ' ' + last;
+// 等同于
+function full (person) {
+  return person.first + ' ' + person.last;
+}
+// this的指向
+function foo () {
+  setTimeout(() => {
+    console.log('id：', this.id)
+  }, 100)
+}
+var id = 21;
+foo.call({id: 42}) // id: 42
+// 如果普通函数，执行时this指向全局window,但箭头函数导致this总是指向函数定义生效时所在的对象
+// 函数绑定
+// foo::bar;  => bar.bind(foo)
+
+// 尾调用，函数最后一步调用另一个函数
+function f(x) {
+  return g(x)
+}
+// es6允许直接写入方法或变量
+var foo = 'bar'
+var baz = {foo}
+bar//{foo: 'bar'}
+// 等同于
+var baz = {foo: foo}
+// ES6允许在对象之中，只写属性名，不写属性值
+function f(x, y) {
+return {x, y};
+}
+// 等同于
+function f(x, y) {
+  return {x: x, y: y};
+  }
+  // p93
+  Object.assign(target, souce1, souce2)
+  // Object.assign浅拷贝
+  // 克隆对象
+  function clone (origin) { // 只能克隆原始对象自身值，不能克隆它继承的值
+    return Object.assign({}, origin)
+  }
+
+  function cloneInherit (origin) {
+    let origniProto = Object.getPrototypeOf(origin)
+    return Object.assign(Object.create(origniProto), origin)
+  }
+
+  // 属性的遍历
+  // (1) for in 
+  // (2) Object.keys(obj)
+  // (3) Object.getOwnPropertyNames(obj)
+  // Symbol,新增原始数据类型
+
+  // P117
+  // Proxy和Reflect  // p128
+  // Reflect与Proxy一样，ES6操作对象的新API
+  // (1)将Object对象的一些明显属于语言内部的方法（比如Object.defineProperty），放到Reflect对象上
+  // (2)修改某些Object方法的返回结果，让其变得更合理。比如，Object.defineProperty(obj, name, desc)在无法定义属性时，会抛出一个错误，而Reflect.defineProperty(obj, name, desc)则会返回false。
+  // (3)让Object操作都变成函数行为。某些Object操作是命令式，比如name in obj和delete obj[name]，而Reflect.has(obj, name)和Reflect.deleteProperty(obj, name)让它们变成了函数行为。
+  // (4)Reflect对象的方法与Proxy对象的方法一一对应，只要是Proxy对象的方法，就能在Reflect对象上找到对应的方法。这就让Proxy对象可以方便地调用对应的Reflect方法，完成默认行为，作为修改行为的基础。也就是说，不管Proxy怎么修改默认行为，你总可以在Reflect上获取默认行为
+  // Reflect.has() 和 Reflect.deleteProperty()
+
+// 作为构造函数，Proxy接受两个参数。
+// 第一个是所要代理的目标对象，如果没有proxy的介入，操作原来要访问的就是这个对象
+// 第二个参数就是一个配置对象，对于每一个被代理的操作，需要提供一个对应的处理函数
+var proxy = new Proxy({}, {
+  get: function (target, property) {
+    return 35;
+  }
+})
+let obj = Object.create(proxy)
+obj.time = 35
+// proxy对象是obj对象的原型，所以根据原型链，会在proxy对象上读取该属性，导致被拦截
+// Proxy的拦截预览
+// (1)get(target, propKey, receiver)
+// （2）set(target, propKey, value, receiver)
+// （3）has(target, propKey)
+// （4）deleteProperty(target, propKey)
+// （5）ownKeys(target)
+// （6）getOwnPropertyDescriptor(target, propKey)
+// （7）defineProperty(target, propKey, propDesc)
+// （8）preventExtensions(target)
+// （9）getPrototypeOf(target)
+// （10）isExtensible(target)
+// （11）setPrototypeOf(target, proto)
+// （12）apply(target, object, args) 如proxy(...args)、proxy.call(object, ...args)、proxy.apply(...)。
+// （13）construct(target, args) 拦截Proxy实例作为构造函数调用的操作，比如new proxy(...args)。
+
+// 各种拦截方法
+// P131
+// 二进制数组,arrayBuffer

f03b06c1dcc0251e0b5a92d4de883ee529faf44c
kimigo168 <462494169@qq.com>
2019-10-30 23:47:31 +0800

    1231231

--- react/index.js
+++ react/index.js
@@ -254,3 +254,74 @@ Greeter.defaultProps = {
 render(<Greeter salutation="Hello world"/>, document.getElementById('root'))
 
 // P69
+import React, { Component, PropTypes } from 'react' 
+import { render } from 'react-dom'
+
+class ContactsApp extends Components {
+  constructor (props) {
+    super(props)
+    this.state = {
+      filterText: ''
+    }
+  }
+  handleUserInput (searchTerm) {
+    this.setState({filterText: searchTerm})
+  }
+  render () {
+    return (
+      <div>
+        <SearchBar filterText={this.state.filterText} onUserInput={this.handleUserInput.bind(this)} />
+        <ContactList contacts={this.props.contacts} filterText={this.state.filterText} />
+      </div>
+    )
+  }
+}
+ContactsApp.propTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.object)
+}
+
+class SearchBar extends Component {
+  handleChange (event) {
+    this.props.onUserInput(event.target.value)
+  }
+  render () {
+    return (<input type="search" placeholder="search" value={this.props.filterText} onChange={this.handleChange.bind(this)} />)
+  }
+}
+
+SearchBar.propTypes = {
+  onUserInput: PropTypes.func.isRequired,
+  filterText: PropTypes.string.isRequired
+}
+
+class ContactList extends Component {
+  render () {
+    let filteredContacts = this.props.contacts.filter(
+      (contact) => contact.name.indexOf(this.props.filterText) !== -1
+    )
+    return (
+      <ul>
+        {filteredContacts.map((contact) => <ContactItem key={contact.email} name={contact.name} email={contact.email}/>)}
+      </ul>
+    )
+  }
+}
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.object),
+  filterText: PropTypes.string.isRequired
+}
+
+class ContactItem extends Component {
+  render () {
+    return <li>{this.props.name} - {this.props.email}</li>
+  }
+}
+ContactItem.propTypes = {
+  name: PropType.string.isRequired,
+  email: PropType.string.isRequired
+}
+let contacts = [
+  { name: 'Cassio Zen', email: 'cassiozen@gmail.com'},
+  { name: 'Dan Abramov', email: 'gsds@somewhere.com'}
+]
+render(<ContactApp contacts={contacts} />, document.getElementById('root'))
